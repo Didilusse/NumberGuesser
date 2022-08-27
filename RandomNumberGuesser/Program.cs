b5dbe44c3118e52a16c8d46765b847791c0c2c9a
Adil <adildevgame@gmail.com>
2022-08-27 11:52:58 -0400

    first commit

---
+++ RandomNumberGuesser/Program.cs
@@ -0,0 +1,34 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+
+Random r = new Random();
+int winNumber = r.Next(0, 100);
+bool win = false;
+
+do
+{
+    Console.Write("Guess a number between 0 and 100: ");
+    string s = Console.ReadLine();
+
+
+    int i = int.Parse(s);
+
+    if (i > winNumber) {
+        Console.WriteLine("Too High!");
+    }
+    else if (i < winNumber) {
+        Console.WriteLine("Too Low!");
+    }
+    else if (i == winNumber)
+    {
+        Console.WriteLine("You Win!");
+        win = true;
+    }
+
+    Console.WriteLine();
+
+} while (win == false);
+Console.WriteLine("Thank you for playing the game");
+Console.Write("Press any key to finish.");
+Console.ReadKey(true);
+
